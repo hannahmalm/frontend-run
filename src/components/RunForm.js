06c7f5f68bfedabdbbf248c9450b981da2b8b3eb
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 10:12:40 -0600

    added in class run form componenet and functional run list component

---
+++ src/components/RunForm.js
@@ -0,0 +1,15 @@
+//new run form 
+import React from "react";
+
+//class component - Will take in State
+class RunForm extends React.Component{
+    render(){
+        return(
+            <div>
+                RunForm
+            </div>
+        )
+    }
+}
+
+export default RunForm;
---
+++ src/components/RunList.js
@@ -0,0 +1,14 @@
+import React from "react";
+//functional componenet - stateless; presents a list of run types
+//pass the props in as an argument
+//you do NOT need a render in a funcitonal component
+
+const RunList = (props) => {
+    return (
+        <div>
+            RunList
+        </div>
+    )
+}
+
+export default RunList;
