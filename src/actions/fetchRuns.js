19aa90e3d2ce903144b9f3d22814519096b7093d
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 10:20:20 -0600

    adding in fetch to fetch all the runs from the api

--- src/actions/fetchRuns.js
+++ src/actions/fetchRuns.js
@@ -1,7 +1,9 @@
 //action creator is a function
 
-export function fetchRuns(action){
-    return action
+export function fetchRuns(){
+  fetch('http://localhost:3000/api/v1/runs')
+  .then(response => response.json())
+  .then(data => console.log(data))
 }
 
 //dispatch action object to reducer which will update state
