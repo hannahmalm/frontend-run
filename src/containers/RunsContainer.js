aa79a448a28f9646eef611e407eef0427cbab641
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 10:14:13 -0600

    updated run container for the run form and the run list

--- src/containers/RunsContainer.js
+++ src/containers/RunsContainer.js
@@ -1,13 +1,17 @@
 //render other components
 //class componenets
 import React from "react";
+import RunForm from "../components/RunForm";
+import RunList from "../components/RunList";
 
 //a class component must have a render 
 class RunsContainer extends React.Component{
     render() {
         return(
-            <div>   
-                RunsContainer
+            <div> 
+                In My Run Container I see:
+                <RunForm></RunForm> 
+                <RunList></RunList> 
             </div>
         )
     }
