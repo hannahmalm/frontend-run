1a4b8f966690878cb1f4cc9c269ac4cda14e26ce
Hannah Malm <hannahemalm616@gmail.com>
2022-02-22 20:33:52 -0600

    added in all redux packages needed

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import React, { Component } from 'react'
 
 class App extends React.Component {
 
-
+//----TEST THE FETCH------------------------
 //test fetch - asynch, request will take some time, dont do anything with the data until you get respopnse bacek
 //fetch request returns a promise which is a promise that it will bring back data
-componentDidMount() {
-  fetch('http://localhost:3000/api/v1/runs')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  // .then(data => console.log(data[1]))
+// componentDidMount() {
+//   fetch('http://localhost:3000/api/v1/runs')
+//   .then(response => response.json())
+  // .then(data => console.log(data))
+  // .then(data => console.log(data[0]))
   // .then(data => console.log(data[0].category))
   //adda serilizer to get this info
   // .then(data => console.log(data[0].logs))
-
-}
+// }
 
 
   render() {
