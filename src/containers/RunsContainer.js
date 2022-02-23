d530219d70636fa8acd217f04ddf9f1ee2764a5b
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 10:03:27 -0600

    added in runs container

---
+++ src/containers/RunsContainer.js
@@ -0,0 +1,14 @@
+//render other components
+//class componenets
+import React from "react";
+
+//a class component must have a render 
+class RunsContainer extends React.Component{
+    render() {
+        return(
+            <div>   
+                RunsContainer
+            </div>
+        )
+    }
+}
