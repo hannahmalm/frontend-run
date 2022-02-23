e3ff4cdc56e9eddb771702bbb9818743a48523ac
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 11:53:31 -0600

    adding in a form to run list

--- src/components/RunForm.js
+++ src/components/RunForm.js
@@ -2,11 +2,30 @@
 import React from "react";
 
 //class component - Will take in State
+//CONTROLLED FORM need a value
+//NEED AN ONCHANGE HANDLER
 class RunForm extends React.Component{
+
+    state = {category: ''}
+
+    //onchange
+    //pass in event as paramenter
+
+    handleOnChange = () => {
+
+    }
+    
+
+    //onsubmit
+
     render(){
         return(
             <div>
-                RunForm
+                <h3>Create a New Training Category</h3>
+                <form>
+                    <input type="text" placeholder="Run Category" value={this.state.category} onChange={this.handleOnChange}/>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
