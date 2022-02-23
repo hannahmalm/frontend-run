305e540f821c244a9da07a7c772527c16783f392
Hannah Malm <hannahemalm616@gmail.com>
2022-02-23 12:04:26 -0600

    added on change logic for form

--- src/components/RunForm.js
+++ src/components/RunForm.js
@@ -10,20 +10,26 @@ class RunForm extends React.Component{
 
     //onchange
     //pass in event as paramenter
-
-    handleOnChange = () => {
-
+    //event.target.value shows what you are typing in
+    handleOnChange = (event) => {
+        this.setState({
+            // category: event.target.value
+            [event.target.name]: event.target.value
+        })
     }
     
 
-    //onsubmit
+    //onsubmit --> Put this data into the database and render into the run list
+    handleOnSubmit = (event) => {
+        debugger;
+    }
 
     render(){
         return(
             <div>
                 <h3>Create a New Training Category</h3>
-                <form>
-                    <input type="text" placeholder="Run Category" value={this.state.category} onChange={this.handleOnChange}/>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input type="text" placeholder="Run Category" value={this.state.category} name="category" onChange={this.handleOnChange}/>
                     <input type="submit" />
                 </form>
             </div>
