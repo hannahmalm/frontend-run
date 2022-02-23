7ae5cc039f1e5f2a12332ae1a07037eacee7f565
Hannah Malm <hannahemalm616@gmail.com>
2022-02-22 19:49:50 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+  }
+}
+
+export default App;
