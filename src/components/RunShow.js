8acc8995ee8da94292c1224741d1cafa856744a9
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 16:01:05 -0600

    added in switch

--- src/components/RunShow.js
+++ src/components/RunShow.js
@@ -3,6 +3,8 @@
 //Import the RunList --> The list of all Runs
 //iterate overall of the Run types on the list --> Each run will have an ID and create a component for  each 
 //each prop passed in should be a SPECIFIC Run type
+import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 const RunShow = (props) => {
     console.log(props)
@@ -10,8 +12,10 @@ const RunShow = (props) => {
     // let run = props.run //this is an array of ALL runs
     let run = props.runs[props.match.params.id - 1] //accessing specific element in array
     // console.log(run)
+    //if run is null, redirect to runs
     return(
         <li>
+            {/* {run ? null : <Redirect to='/runs/'/>} */}
             {run ? run.category :null}
             {/* {run.category} */}
       
--- src/containers/RunsContainer.js
+++ src/containers/RunsContainer.js
@@ -3,7 +3,7 @@
 import App from "../App";
 import React from "react";
 import {connect} from 'react-redux'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import RunForm from "../components/RunForm";
 import RunList from "../components/RunList";
 import {fetchRuns} from '../actions/fetchRuns.js'
@@ -28,13 +28,17 @@ componentDidMount(){
 //runs component is iterating over collection of run types --> give run component the props for runs list
 //router props need to be passed in when rendering a specific id --> DONT use componenet, use render instead
 //routerProps automatically adds in the props to the component we are renderign
+//Switch returns the FIRST path that matches --> SPECIFIC routes must come before any dynamic routes
+//exact does not work fro runs/new and /runs/:id because it technically is the exact path
     render() {
         return(
         <div>
+            <Switch>
         <Route exact path='/runs/new' component={RunForm}/>
         <Route exact path='/runs/:id' render={(routerProps)=> <RunShow {...routerProps} runs={this.props.runs}/>}  />
         <Route exact path='/runs' render={(routerProps)=> <RunList {...routerProps} runs={this.props.runs}/>} />
         {/* <RunList runs={this.props.runs}/> */}
+        </Switch>
         </div>
         )
     }
