3ba9175cbb9466c7d658b5208d40a63b2b8cafc7
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 16:07:58 -0600

    added basic log form and render it in log container

---
+++ src/components/LogForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//forms should be a class componenet because you need to keep track of entered values
+//in order to have a contolled form you need to store input values in the state
+
+class LogForm extends React.Component {
+    render() {
+        return(
+            <div>
+                Log Form 
+            </div>
+        )
+    }
+}
+
+
+export default LogForm;
--- src/containers/LogsContainer.js
+++ src/containers/LogsContainer.js
@@ -1,11 +1,12 @@
 //Typically containers have state with multiple functions
 import React from 'react'
+import LogForm from '../components/LogForm';
 
 class LogsContainer extends React.Component {
     render() {
         return(
             <div>
-                logs container
+                <LogForm/>
             </div>
         )
     }
