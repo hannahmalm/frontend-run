40be606d5eade3285e4a0048998d82819e8cc02c
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 16:28:38 -0600

    imported Log LIst into the logs container

--- src/containers/LogsContainer.js
+++ src/containers/LogsContainer.js
@@ -1,12 +1,14 @@
 //Typically containers have state with multiple functions
 import React from 'react'
 import LogForm from '../components/LogForm';
+import LogsList from '../components/LogList';
 
 class LogsContainer extends React.Component {
     render() {
         return(
             <div>
                 <LogForm/>
+                <LogsList/>
             </div>
         )
     }
