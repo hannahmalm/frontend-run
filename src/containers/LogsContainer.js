1584bec07ed09851828bc6dd0d7872a8d5a6a25a
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 16:04:00 -0600

    added in basic logs container

---
+++ src/containers/LogsContainer.js
@@ -0,0 +1,13 @@
+//Typically containers have state with multiple functions
+import React from 'react'
+
+class LogsContainer extends React.Component {
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export default LogsContainer;
