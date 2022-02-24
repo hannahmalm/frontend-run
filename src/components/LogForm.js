f03fb0d23176dc7be557385d501d4920e580fbf9
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 17:26:00 -0600

    added in Run form

--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -2,16 +2,47 @@ import React from "react";
 
 //forms should be a class componenet because you need to keep track of entered values
 //in order to have a contolled form you need to store input values in the state
+//can get access to all props so no need to fetch --> need a submit and then connect to the store 
+//connect compoenet so that when you call it will send dispatch
+//How do you make a controlled form? Add Values and Names that correspond to keys in DB and the form holds values in the local state
+import {connect} from 'react-redux'
 
 class LogForm extends React.Component {
+
+    //controlled values in local state
+    //name in form must match key in state
+    //state is updated using handle onChange
+    state = {
+        distance: '',
+        pace: '',
+        date: '',
+        notes: ''
+
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     render() {
         return(
             <div>
-                Log Form 
+              <h3>Create a Run Log</h3>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input  type="number" step="0.01" placeholder="Distance" value={this.state.distance} name="distance" onChange={this.handleOnChange}/>
+                    <input type="text" placeholder="Pace" value={this.state.pace} name="pace" onChange={this.handleOnChange}/>
+                    <input type="date" placeholder="Date" value={this.state.date} name="date" onChange={this.handleOnChange}/>
+                    <input type="text" placeholder="Notes" value={this.state.notes} name="notes" onChange={this.handleOnChange}/>
+                    <input type="submit" />
+                </form>
             </div>
+    
         )
     }
 }
 
 
-export default LogForm;
+export default connect(null) (LogForm);
