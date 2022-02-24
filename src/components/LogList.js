7e1a97168d19b29463cc0f72c3393faf1630e772
Hannah Malm <hannahemalm616@gmail.com>
2022-02-24 16:28:02 -0600

    created a log list functional component

---
+++ src/components/LogList.js
@@ -0,0 +1,14 @@
+//render all logs --> Functional compoenent because only rendering 
+//functional components receive props
+import React from "react";
+
+const LogList = (props) => {
+    return(
+        <div>
+            LogList
+        </div>
+    )
+}
+
+
+export default LogsList;
