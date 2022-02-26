448cea577d5f71c1842cade0d08261d3876fb4cf
Hannah Malm <hannahemalm616@gmail.com>
2022-02-25 20:13:24 -0600

    added in footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <nav class="navbar fixed-bottom navbar-light bg-light">
+            <a class="navbar-brand" href="#">Fixed bottom</a>
+            </nav>
+
+
+
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/RunList.js
+++ src/components/RunList.js
@@ -9,6 +9,13 @@ import {Route, Link} from 'react-router-dom'
 //props is the info being passed from run container to child component of runs
 const RunList = (props) => {
     return (
+
+//         <div className="card" >
+//         <div className="card-body">
+//             {/* <h2 className="card-title">Benefits of Running</h2>
+//             <li className="benefits-list">Running Builds Muscular Strength.</li */}
+//         </div>
+// </div>
         <div>
            {props.runs.map(run => <div key={run.id}>
             <Link to={`/runs/${run.id}`}>{run.category}</Link> </div>)}  
