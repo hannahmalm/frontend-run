583a9fb5209aba964153174a01eebc5a9d61c1a5
Hannah Malm <hannahemalm616@gmail.com>
2022-03-01 14:52:07 -0600

    pasted in bootstrap header

--- src/components/RunList.js
+++ src/components/RunList.js
@@ -6,6 +6,19 @@ import {Route, Link} from 'react-router-dom'
 //you do NOT need a render in a funcitonal component
 //Return entire list of Runs
 
+
+<div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold">Centered hero</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+      </div>
+    </div>
+  </div>
+
+
 //props is the info being passed from run container to child component of runs
 const RunList = (props) => {
     return (
@@ -16,10 +29,10 @@ const RunList = (props) => {
 //             <li className="benefits-list">Running Builds Muscular Strength.</li */}
 //         </div>
 // </div>
-        <div>
+         <div> 
            {props.runs.map(run => <div key={run.id}>
             <Link to={`/runs/${run.id}`}>{run.category}</Link> </div>)}  
-        </div>
+      </div> 
     )
 }
 
