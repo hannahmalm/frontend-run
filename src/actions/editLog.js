166037b599005e88af9def5802d32b1e39a1688f
Hannah Malm <hannahemalm616@gmail.com>
2022-03-04 14:45:41 -0600

    adding in a new log edit form

--- src/actions/editLog.js
+++ src/actions/editLog.js
@@ -1,7 +1,11 @@
 export const editLog = (logId,runId) => {
     return (dispatch) => {
         return fetch(`http://localhost:3000/api/v1/runs/${runId}/logs/${logId}`, {
-            method: 'UPDATE'
+            method: 'UPDATE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
         })
         .then(response => response.json())
         .then(run => dispatch({type: 'UPDATE_LOG', payload: run}))
