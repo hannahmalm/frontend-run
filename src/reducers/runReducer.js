243f4ba032eb6f11a9ae51da2030d77138567917
Hannah Malm <hannahemalm616@gmail.com>
2022-03-05 11:19:21 -0600

    cleaning up comments on the reducer

--- src/reducers/runReducer.js
+++ src/reducers/runReducer.js
@@ -13,10 +13,6 @@ export default function runReducer(state = {runs: []}, action) {
         case 'FETCH_RUNS': 
             //return a new version of state
             return {runs: action.payload}
-        // case 'CREATE_RUN':
-        //     //want to keep runs taht are in state AND add a new one --> use state spread
-        //     //runs should point to an array that has all of the previous runs in it, along with the new run
-        //     return {...state, runs: [...state.runs, action.payload]}
             //go through every run, if the run ID is not the same as the one associted with log, replace it
             //iterate over all runs and if the run id = the id that is coming in to the action.paylod, then replace using map to make a new array to action.payload
             //if the run id does not match, return the original run
@@ -24,26 +20,16 @@ export default function runReducer(state = {runs: []}, action) {
             //find the run ID thats being passed in, if it matches the payload id
             //return a new array. Look through all the runs 
             // if the run id = the action payload id, then return the new array using action.payload (updated run array)
-            //map will create a NEW array of the runs with the new and updated runs
-            //else return the original run arary 
         case 'CREATE_LOG':
-           let runs = state.runs.map(run => {
+           let runs = state.runs.map(run => {   //map will create a NEW array of the runs with the new and updated runs
                if (run.id === action.payload.id ){
                    return action.payload
                }
                else {
-                   return run
+                   return run   //else return the original run array
                }
            })
            return {...state, runs: runs} //this will update the page and actually render the new log on the runs page
-            // return {...state, runs: state.runs.map(run => {
-            //     if (run.id === action.payload.id) {
-            //         return action.payload
-            //     }
-            //     else {
-            //     return run 
-            //     }
-            // })}
            //the run we are sending back is the run associated with the log
            // you have to do it this way due to nesting on the backend 
         case 'DELETE_LOG':
@@ -55,7 +41,7 @@ export default function runReducer(state = {runs: []}, action) {
                     return run
                 }
             })
-            return {...state, runs: runsDelete}
+            return {...state, runs: runsDelete} //return a copy of state and then delete out the log
     default:
         return state
     }
