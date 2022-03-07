64fd83b072097175a48ebcb0c2ad68d6791837a2
Hannah Malm <hannahemalm616@gmail.com>
2022-03-07 16:24:50 -0600

    upated notes on run list header

--- src/components/RunListHeader.js
+++ src/components/RunListHeader.js
@@ -5,19 +5,14 @@ import React from 'react'
 const RunListHeader = (props) => {
     return(
         <div className="header-text-center">
-            <br></br>
-            <br></br>
-        <h1 className="display-5 fw-bold">All Run Journals</h1>
+                <br></br>
+                <br></br>
+                <h1 className="display-5 fw-bold">All Run Journals</h1>
             <div className="col-lg-6 mx-auto">
                 <p className="lead mb-4">Quickly access all of your run logs based on run type. <br></br> <b>Click on each link to see previous run journals or create a new run journal.</b> Logging each run allows you to track your progress and see diversification in training methods. Each link contains instructions and a video overview of the run category.</p>
-           </div>
-           </div>
-        
-       
+            </div>
+        </div>
     )
-
-  
-
 }
 
 export default RunListHeader;
